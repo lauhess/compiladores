cdf172c34e674944c6da52b4dc6792cd5cdc4c87
Lab01 <you@example.com>
2023-10-03 18:45:14 -0300

    Se sigue run bytecode

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -25,13 +25,14 @@ import Data.Binary.Get ( getWord32le, isEmpty )
 
 import Data.List (intercalate)
 import Data.Char
+import Eval (semOp)
 
 type Opcode = Int
 type Bytecode = [Int]
 
 newtype Bytecode32 = BC { un32 :: [Word32] }
 
-type Env = [Int]
+type Env = [Val]
 data Val = I Int | Fun Env Bytecode | RA Env Bytecode
 
 {- Esta instancia explica como codificar y decodificar Bytecode de 32 bits -}
@@ -103,31 +104,31 @@ showBC = intercalate "; " . showOps
 
 -- Compila un término a bytecode
 bcc :: MonadFD4 m => TTerm -> m Bytecode
-bcc t = case t of 
+bcc t = case t of
   V _ _ -> failFD4 "implementame!"
   Const _ (CNat n) -> return [CONST, fromIntegral n]
-  Lam _ _ _ (Sc1 t1) -> do 
+  Lam _ _ _ (Sc1 t1) -> do
     bt <- bcc t
     return $ [FUNCTION, length bt] ++ bt ++ [RETURN]
-  App _ t1 t2 -> do 
+  App _ t1 t2 -> do
     b1 <- bcc t1
     b2 <- bcc t2
     return $ b1 ++ b2 ++ [CALL]
-  Print _ str t1 -> do 
-    bt <- bcc t 
-    return $ [length str] ++ string2bc str ++ [PRINT] ++ bt ++ [PRINTN] 
-  BinaryOp _ Add t1 t2 -> do 
+  Print _ str t1 -> do
+    bt <- bcc t
+    return $ [length str] ++ string2bc str ++ [PRINT] ++ bt ++ [PRINTN]
+  BinaryOp _ Add t1 t2 -> do
     b1 <- bcc t1
     b2 <- bcc t2
     return $ b1 ++ b2 ++ [ADD]
-  BinaryOp _ Sub t1 t2 -> do 
+  BinaryOp _ Sub t1 t2 -> do
     b1 <- bcc t1
-    b2 <- bcc t2  
+    b2 <- bcc t2
     return $ b1 ++ b2 ++ [SUB]
-  Fix _ _ _ _ _ (Sc2 t1) -> do 
+  Fix _ _ _ _ _ (Sc2 t1) -> do
     b1 <- bcc t1
     return $ [FUNCTION, length b1] ++ b1 ++ [FIX]
-  IfZ _ c t1 t2 -> do 
+  IfZ _ c t1 t2 -> do
     bc <- bcc c
     b1 <- bcc t1
     b2 <- bcc t2
@@ -140,11 +141,11 @@ bcc t = case t of
     -- - Sino, hacemos un salto de n + 1 (JUMP y el largo de b1), y ejecutamos
     --   la condición del else.
     -- ToDo: Pensar casos de ifz anidados
-  Let _ _ _ t1 (Sc1 t2) -> do 
+  Let _ _ _ t1 (Sc1 t2) -> do
     b1 <- bcc t1
     b2 <- bcc t2
     return $ b1 ++ [SHIFT] ++ b2 ++ [DROP]
-  
+
 -- ord/chr devuelven los codepoints unicode, o en otras palabras
 -- la codificación UTF-32 del caracter.
 string2bc :: String -> Bytecode
@@ -153,7 +154,6 @@ string2bc = map ord
 bc2string :: Bytecode -> String
 bc2string = map chr
 
-type Module = ()
 bytecompileModule :: MonadFD4 m => Module -> m Bytecode
 bytecompileModule m = failFD4 "implementame!"
 
@@ -171,17 +171,29 @@ bcRead filename = (map fromIntegral <$> un32) . decode <$> BS.readFile filename
 
 
 runBC :: MonadFD4 m => Bytecode -> m ()
-runBC bc = void $ runBC' bc [] [] 
+runBC bc = void $ runBC' bc [] []
 
-runBC' :: MonadFD4 m => Bytecode -> [Val] -> [Val] -> m Val
-runBC' bs e c = case bs of
-  []              -> (return . head) c
-  (NULL:xs)       -> runBC' xs e c
-  (RETURN:xs)     -> let (val : RA e' bs' : c') = c
-                      in runBC' bs' e (val : c')
-  (CONST:i:xs)    -> runBC' xs e (I i:c)
-  (ACCESS:i:xs)   -> runBC' xs e (e !! i : c)
+runBC' :: MonadFD4 m => Bytecode -> Env -> [Val] -> m Val
+runBC' bs e s = case bs of
+  []              -> (return . head) s
+  (NULL:xs)       -> runBC' xs e s
+  (RETURN:xs)     -> let (val : RA e' bs' : s') = s
+                      in runBC' bs' e (val : s')
+  (CONST:i:xs)    -> runBC' xs e (I i:s)
+  (ACCESS:i:xs)   -> runBC' xs e ( e !! i : s)
   (FUNCTION:i:xs) -> let bs' = take i xs
-                      in runBC' xs e (Fun e bs' : c)
-  _               -> undefined
-  
+                      in runBC' xs e (Fun e bs' : s)
+  (CALL:xs)       -> let (v: (Fun ef cf) : s') = s
+                      in runBC' cf (v:ef) (RA e xs:s)
+  (ADD:xs)        -> let (I x : I y : s') = s
+                      in runBC' xs e (I (semOp Add x y):s')
+  (SUB:xs)        -> let (I x : I y : s') = s
+                      in runBC' xs e (I (semOp Sub x y):s')
+  (FIX:xs)        -> undefined
+  (STOP:xs)       -> undefined
+  (JUMP:i:xs)     -> undefined
+  (SHIFT:xs)      -> runBC' xs (head s : e) (tail s)
+  (DROP:xs)       -> runBC' xs (tail e) s
+  (PRINT:xs)      -> undefined
+  (PRINTN:xs)     -> undefined
+  (x:xs)          -> undefined
