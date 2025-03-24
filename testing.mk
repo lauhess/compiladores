1afa7ac43fc7ce0109ba7e873b4b2bc5fa48df46
Laureano Hess <laureanohess0@gmail.com>
2025-03-23 23:02:54 -0300

    Se agrega test valores maximos y unicode

--- testing.mk
+++ testing.mk
@@ -1,6 +1,7 @@
 # TESTDIRS += tests/ok/00-basicos
 TESTDIRS += tests/ok/10-sugar
 TESTDIRS += tests/ok/20-tysym
+TESTDIRS += tests/ok/30-otros
 
 TESTS	:= $(shell find $(TESTDIRS) -name '*.fd4' -type f | sort)
 
